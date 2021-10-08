3b9bc8b75ea6e22a1e78ed32ff2e84a5094e9adc
Gerardo Leal <gleal@atlassian.com>
2021-10-08 15:31:23 +1100

    final demo ready

---
+++ src/machines/final/index.ts
@@ -0,0 +1,95 @@
+import { createMachine, assign } from "xstate";
+
+type MachineContext = {
+  lives: number;
+};
+
+type MachineEvent =
+  | { type: "IDLE" }
+  | { type: "WALK" }
+  | { type: "RUN" }
+  | { type: "JUMP" }
+  | { type: "DIE" };
+
+export const FinalDinoMachine = createMachine<MachineContext, MachineEvent>({
+  id: "DINO_MACHINE",
+
+  initial: "Idle",
+
+  context: {
+    lives: 3,
+  },
+
+  states: {
+    Idle: {
+      on: {
+        WALK: { target: "Walking" },
+      },
+    },
+
+    Walking: {
+      on: {
+        IDLE: { target: "Idle" },
+        RUN: { target: "Running" },
+      },
+    },
+
+    Running: {
+      on: {
+        WALK: { target: "Walking" },
+        DIE: { target: "Dead" },
+        JUMP: { target: "Jumping" },
+      },
+    },
+
+    Jumping: {
+      after: {
+        1000: { target: "Running" },
+      },
+      entry: [() => console.log("started jumping")],
+      exit: [() => console.log("finished jumping")],
+      //   on: {
+      //     RUN: { target: "Running" },
+      //   },
+      //   invoke: {
+      //     //   src: () => (send) => {
+      //     //     setTimeout(() => send("RUN"), 1000);
+      //     //   },
+      //     src: () =>
+      //       new Promise((resolve) => {
+      //         setTimeout(() => resolve(true), 1000);
+      //       }),
+      //     onDone: {
+      //       target: "Running",
+      //     },
+      //   },
+    },
+
+    Dead: {
+      on: {
+        IDLE: {
+          target: "Idle",
+          cond: (context) => {
+            const hasLivesLeft = context.lives >= 0;
+
+            if (!hasLivesLeft) {
+              alert(
+                "You ran out of lives Dino... Refresh the page to play again"
+              );
+            }
+
+            return hasLivesLeft;
+          },
+        },
+      },
+      invoke: {
+        src: () => Promise.resolve(),
+        onDone: {
+          actions: assign({
+            lives: (context) => context.lives - 1,
+          }),
+        },
+      },
+    },
+  },
+});
--- src/pages/final/index.tsx
+++ src/pages/final/index.tsx
@@ -1,9 +1,41 @@
 import * as React from "react";
+import { useMachine } from "@xstate/react";
 
-const FinalPage = () => (
-  <>
-    <h2>Final Page</h2>
-  </>
-);
+import { FinalDinoMachine } from "../../machines/final";
+
+import { Dino, DinoState } from "../../components";
+
+const FinalPage = () => {
+  const [state, send] = useMachine(FinalDinoMachine, {
+    devTools: true,
+  });
+
+  return (
+    <>
+      <h2>Final Page</h2>
+
+      <h4>
+        Lives:{" "}
+        {Array.from({ length: state.context.lives }).map((val, key) => (
+          <span key={key}>💚</span>
+        ))}
+      </h4>
+
+      <div>
+        <button onClick={() => send("IDLE")}>Idle</button>
+
+        <button onClick={() => send("WALK")}>Walk</button>
+
+        <button onClick={() => send("RUN")}>Run</button>
+
+        <button onClick={() => send("JUMP")}>Jump</button>
+
+        <button onClick={() => send("DIE")}>Die</button>
+      </div>
+
+      <Dino state={state.value.toString() as DinoState} />
+    </>
+  );
+};
 
 export default FinalPage;
